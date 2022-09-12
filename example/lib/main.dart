bd551a694efdafb5ee81a098fccba90bccffdf7b
Luis Reinier Ferreiro Vázquez <lrferreiro910208@gmail.com>
2022-09-11 23:45:48 -0400

    Updated project

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,35 +2,65 @@ import 'package:flutter/material.dart';
 import 'package:flutter_swipe_button/flutter_swipe_button.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
@@ -38,42 +68,43 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Padding(
-              padding: EdgeInsets.symmetric(
+              padding: const EdgeInsets.symmetric(
                 horizontal: 30,
                 vertical: 8,
               ),
               child: SwipeButton.expand(
-                thumb: Icon(
+                thumb: const Icon(
                   Icons.double_arrow_rounded,
                   color: Colors.white,
-                ),
-                child: Text(
-                  "Swipe to ...",
-                  style: TextStyle(
-                    color: Colors.red,
-                  ),
                 ),
                 activeThumbColor: Colors.red,
                 activeTrackColor: Colors.grey.shade300,
                 onSwipe: () {
                   ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
+                    const SnackBar(
                       content: Text("Swipped"),
                       backgroundColor: Colors.green,
                     ),
                   );
                 },
+                child: const Text(
+                  "Swipe to ...",
+                  style: TextStyle(
+                    color: Colors.red,
+                  ),
+                ),
               ),
             ),
             Padding(
-              padding: EdgeInsets.symmetric(
+              padding: const EdgeInsets.symmetric(
                 horizontal: 30,
                 vertical: 8,
               ),
               child: SwipeButton(
-                trackPadding: EdgeInsets.all(6),
-                elevation: 2,
-                child: Text(
+                trackPadding: const EdgeInsets.all(6),
+                elevationThumb: 2,
+                elevationTrack: 2,
+                child: const Text(
                   "Swipe to ...",
                   style: TextStyle(
                     color: Colors.white,
@@ -81,7 +112,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 onSwipe: () {
                   ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
+                    const SnackBar(
                       content: Text("Swipped"),
                       backgroundColor: Colors.green,
                     ),
@@ -90,20 +121,21 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             Padding(
-              padding: EdgeInsets.symmetric(
+              padding: const EdgeInsets.symmetric(
                 horizontal: 30,
                 vertical: 8,
               ),
               child: SwipeButton(
-                thumbPadding: EdgeInsets.all(3),
-                thumb: Icon(
+                thumbPadding: const EdgeInsets.all(3),
+                thumb: const Icon(
                   Icons.chevron_right,
                   color: Colors.white,
                 ),
-                elevation: 2,
+                elevationThumb: 2,
+                elevationTrack: 2,
                 child: Text(
                   "Swipe to ...".toUpperCase(),
-                  style: TextStyle(
+                  style: const TextStyle(
                     color: Colors.white,
                     fontSize: 18,
                     fontWeight: FontWeight.bold,
@@ -111,7 +143,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 onSwipe: () {
                   ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
+                    const SnackBar(
                       content: Text("Swipped"),
                       backgroundColor: Colors.green,
                     ),
@@ -120,7 +152,7 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             Padding(
-              padding: EdgeInsets.symmetric(
+              padding: const EdgeInsets.symmetric(
                 horizontal: 30,
                 vertical: 8,
               ),
@@ -128,7 +160,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 borderRadius: BorderRadius.circular(8),
                 activeTrackColor: Colors.amber,
                 height: 60,
-                child: Text(
+                child: const Text(
                   "Swipe to ...",
                   style: TextStyle(
                     color: Colors.red,
@@ -136,7 +168,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 onSwipe: () {
                   ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
+                    const SnackBar(
                       content: Text("Swipped"),
                       backgroundColor: Colors.green,
                     ),
@@ -145,7 +177,7 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             Padding(
-              padding: EdgeInsets.symmetric(
+              padding: const EdgeInsets.symmetric(
                 horizontal: 30,
                 vertical: 8,
               ),
@@ -154,7 +186,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 activeThumbColor: Colors.yellow,
                 borderRadius: BorderRadius.zero,
                 height: 30,
-                child: Text(
+                child: const Text(
                   "Swipe to ...",
                   style: TextStyle(
                     color: Colors.white,
@@ -162,7 +194,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 onSwipe: () {
                   ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
+                    const SnackBar(
                       content: Text("Swipped"),
                       backgroundColor: Colors.green,
                     ),
@@ -171,13 +203,13 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             Padding(
-              padding: EdgeInsets.symmetric(
+              padding: const EdgeInsets.symmetric(
                 horizontal: 30,
                 vertical: 8,
               ),
               child: SwipeButton(
                 width: 200,
-                child: Text(
+                child: const Text(
                   "Swipe to ...",
                   style: TextStyle(
                     color: Colors.white,
@@ -185,7 +217,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 onSwipe: () {
                   ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
+                    const SnackBar(
                       content: Text("Swipped"),
                       backgroundColor: Colors.green,
                     ),
